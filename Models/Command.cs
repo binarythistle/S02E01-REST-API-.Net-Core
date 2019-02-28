e5e100a99b4508a33ff0cfd3f0f96bda92fb147b
les jackson <les.jackson@binarythistle.com>
2019-02-28 19:21:12 +1100

    4th commit

--- Models/Command.cs
+++ Models/Command.cs
@@ -2,6 +2,8 @@ namespace CommandAPI.Models
 {
     public class Command
     {
+        //test comment
+
         public int Id {get; set;}
         public string Howto {get; set;}
         public string Platform {get; set;}
