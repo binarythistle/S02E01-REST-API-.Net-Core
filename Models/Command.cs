542035a3c94603c1df0a9d1a53b408e753ae034e
les jackson <les.jackson@binarythistle.com>
2019-02-28 19:34:07 +1100

    test trigger

--- Models/Command.cs
+++ Models/Command.cs
@@ -3,6 +3,8 @@ namespace CommandAPI.Models
     public class Command
     {
 
+        //Comment set after pulling azure-pipelines.yaml
+
         public int Id {get; set;}
         public string Howto {get; set;}
         public string Platform {get; set;}
